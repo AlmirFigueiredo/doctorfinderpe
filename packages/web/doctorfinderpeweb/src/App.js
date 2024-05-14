5166a61c37950b9fb5117f6d7872344b6bb89aff
Jose Almir <joseamfj@outlook.com>
2024-05-14 01:17:32 -0300

    Create connection between frontend and backend

--- packages/web/doctorfinderpeweb/src/App.js
+++ packages/web/doctorfinderpeweb/src/App.js
@@ -1,22 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
 
 function App() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch('https://docfserver-df4bbe38343d.herokuapp.com/users')
+      .then(response => response.json())
+      .then(data => setUsers(data))
+      .catch(error => console.error('Erro ao buscar usuários:', error));
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Usuários</h1>
+        <ul>
+          {users.map(user => (
+            <li key={user.id}>{user.user_name}</li>
+          ))}
+        </ul>
       </header>
     </div>
   );
