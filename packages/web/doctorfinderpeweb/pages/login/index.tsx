02337afa4e372a66d2301a88f08364a74d60d585
Marcelo <mhbcoelho99@gmail.com>
2024-05-17 15:00:13 -0300

    nextjs config done

---
+++ packages/web/doctorfinderpeweb/pages/login/index.tsx
@@ -0,0 +1,12 @@
+import styles from "./login.module.css"
+
+export default function Login() {
+    return (
+        <>
+            <header className={styles.header}>
+                <h1>DoctorFinder</h1>
+            </header>
+        
+        </>
+    )
+}
