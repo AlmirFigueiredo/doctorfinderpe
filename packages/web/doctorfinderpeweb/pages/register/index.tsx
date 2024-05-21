71d88fd9b20765c645b7658079c8738aa39fa3ab
Marcelo <mhbcoelho99@gmail.com>
2024-05-21 09:34:19 -0300

    ts implementation

--- packages/web/doctorfinderpeweb/pages/register/index.tsx
+++ packages/web/doctorfinderpeweb/pages/register/index.tsx
@@ -1,111 +1,164 @@
 "use client"
-import React from "react"
-import styles from "./register.module.css"
+import React, { ChangeEvent, FormEvent, useState } from "react";
+import styles from "./register.module.css";
 
+type InitialFormData = {
+  username: string;
+  email: string;
+  password: string;
+  role: string;
+};
+
+type DoctorFormData = {
+  license: string;
+  CRM: string;
+};
 
 export default function Login() {
-    const [initialFormData, setInitialinitialFormData] = React.useState({
-        username: "",
-        email: "",
-        password: "",
-        role: ""
-    })
+  const [initialFormData, setInitialFormData] = useState<InitialFormData>({
+    username: "",
+    email: "",
+    password: "",
+    role: ""
+  });
 
-    const [doctorInitialFormData, setDoctorinitialFormData] = React.useState({
-        license: "",
-        CRM: ""
-    })
+  const [doctorFormData, setDoctorFormData] = useState<DoctorFormData>({
+    license: "",
+    CRM: ""
+  });
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setInitialFormData((prevState) => ({ ...prevState, [name]: value }));
+  };
+
+  const handleDoctorChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setDoctorFormData((prevState) => ({ ...prevState, [name]: value }));
+  };
+
+  const handleSubmit = async (e: FormEvent) => {
+    e.preventDefault();
     
-
-    const handleChange = (e: any) => {
-        const {name, value} = e.target;
-        setInitialinitialFormData((prevState) => ({...prevState, [name]: value}));
+    let formData = { ...initialFormData };
+    if (initialFormData.role === 'doctor') {
+      formData = { ...formData, ...doctorFormData };
     }
-
-    const handleDoctorChange = (e: any) => {
-        const {name, value} = e.target;
-        setDoctorinitialFormData((prevState) => ({...prevState, [name]: value}));
+    const serializedBody = JSON.stringify(formData);
+    const fetchOptions = {
+      method: 'POST',
+      body: serializedBody,
+      headers: { 'Content-Type': 'application/json' }
+    };
+    
+    try {
+      const response = await fetch('/api', fetchOptions);
+      if (response.ok) {
+        console.log('Success');
+      } else {
+        console.log('Account already created, I suppose');
+      }
+    } catch (error) {
+      console.log(error);
     }
+  };
 
-    const handleSubmit = async (e: any) => {
-        e.preventDefault();
-        
-        let formData = initialFormData;
-        if(initialFormData.role === 'doctor') {
-            formData = {
-                ...formData,
-                ...doctorInitialFormData
-            }
-        }
-        const serializedBody = JSON.stringify(initialFormData);
-        const fetchOptions = {
-            method: 'post',
-            body: serializedBody,
-            headers: { 'Content-Type': 'application/json' }
-        };
-        
-        try {
-            const response = await fetch('/api', fetchOptions);
-
-            if(response.ok) {
-                console.log('success');
-            } else {
-                console.log('account already created i supose');
-            }
-        } catch (error) {
-            console.log(error);
-        }
-    }
-
-    return (
-        <>
-            <header className={styles.header}>
-                <h1>DoctorFinder</h1>
-            </header>
-            <div className={styles.container}>
-                <div className={styles.registerBox}>
-                    <h1>Create Your Account here</h1>
-                    <form onSubmit={handleSubmit}>
-                        <div className={styles.formGroup}>
-                            <input name="username" required value={initialFormData.username} onChange={handleChange} type="text" placeholder="Username"/>
-                        </div>
-                        <div className={styles.formGroup}>
-                            <input name="email" required value={initialFormData.email} onChange={handleChange} type="text" placeholder="E-mail"/>
-                        </div>
-                        <div className={styles.formGroup}>
-                            <input name="password" required value={initialFormData.password} onChange={handleChange} type="password" placeholder="password"/>
-                        </div>
-                        <div className={styles.formGroup}>
-                            <span>Select Your Role</span>
-                            <div>
-                                <label htmlFor="client">Client</label>
-                                <input value="client" required onChange={handleChange} checked={initialFormData.role === "client"} type="radio" name="role" id="client" />
-                            </div>
-                            <div>
-                                <label htmlFor="doctor">Doctor</label>
-                                <input value="doctor" required onChange={handleChange} checked={initialFormData.role === "doctor"} type="radio" name="role" id="doctor" />
-                            </div>
-                        </div>
-                        {
-                            initialFormData.role === "doctor" ? <>
-                                <div className={styles.formGroup}>
-                                    <input name="license" value={doctorInitialFormData.license} required onChange={handleDoctorChange} type="text" placeholder="license"/>
-                                </div>
-                                <div className={styles.formGroup}>
-                                    <input name="CRM" value={doctorInitialFormData.CRM} required onChange={handleDoctorChange} type="text" placeholder="CRM"/>
-                                </div>
-                            </> : ""
-                        }
-                        <button>Register</button>
-                    </form>
-                    
-                    <div className={styles.createAccountLogin}>
-                        <span>Já tem uma conta?</span>
-                        <a href="./login"> Fazer login aqui!</a>
-                    </div>
-                </div>
+  return (
+    <>
+      <header className={styles.header}>
+        <h1>DoctorFinder</h1>
+      </header>
+      <div className={styles.container}>
+        <div className={styles.registerBox}>
+          <h1>Create Your Account here</h1>
+          <form onSubmit={handleSubmit}>
+            <div className={styles.formGroup}>
+              <input
+                name="username"
+                required
+                value={initialFormData.username}
+                onChange={handleChange}
+                type="text"
+                placeholder="Username"
+              />
             </div>
-            
-        </>
-    )
+            <div className={styles.formGroup}>
+              <input
+                name="email"
+                required
+                value={initialFormData.email}
+                onChange={handleChange}
+                type="email"
+                placeholder="E-mail"
+              />
+            </div>
+            <div className={styles.formGroup}>
+              <input
+                name="password"
+                required
+                value={initialFormData.password}
+                onChange={handleChange}
+                type="password"
+                placeholder="Password"
+              />
+            </div>
+            <div className={styles.formGroup}>
+              <span>Select Your Role</span>
+              <div>
+                <label htmlFor="client">Client</label>
+                <input
+                  value="client"
+                  onChange={handleChange}
+                  checked={initialFormData.role === "client"}
+                  type="radio"
+                  name="role"
+                  id="client"
+                />
+              </div>
+              <div>
+                <label htmlFor="doctor">Doctor</label>
+                <input
+                  value="doctor"
+                  onChange={handleChange}
+                  checked={initialFormData.role === "doctor"}
+                  type="radio"
+                  name="role"
+                  id="doctor"
+                />
+              </div>
+            </div>
+            {initialFormData.role === "doctor" && (
+              <>
+                <div className={styles.formGroup}>
+                  <input
+                    name="license"
+                    value={doctorFormData.license}
+                    required
+                    onChange={handleDoctorChange}
+                    type="text"
+                    placeholder="License"
+                  />
+                </div>
+                <div className={styles.formGroup}>
+                  <input
+                    name="CRM"
+                    value={doctorFormData.CRM}
+                    required
+                    onChange={handleDoctorChange}
+                    type="text"
+                    placeholder="CRM"
+                  />
+                </div>
+              </>
+            )}
+            <button type="submit">Register</button>
+          </form>
+          <div className={styles.createAccountLogin}>
+            <span>Já tem uma conta?</span>
+            <a href="./login"> Fazer login aqui!</a>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
