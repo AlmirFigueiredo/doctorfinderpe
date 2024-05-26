6ada831dda3ff16614cdbd362981c87d2fae67bd
Jose Almir <joseamfj@outlook.com>
2024-05-25 22:02:27 -0300

    Create some services and controllers for doctor model

--- packages/server/src/controllers/doctorController.ts
+++ packages/server/src/controllers/doctorController.ts
@@ -1,13 +1,48 @@
 import { Request, Response } from 'express';
-import { getAllDoctors } from '../services/doctorService';
+import { createDoctor, getAllDoctors, getdoctorById, updatedoctor } from '../services/doctorService';
 
-//OBS: NAO EH EFICIENTE ROTA PARA PEGAR TODOS OS USUARIOS, APENAS EXEMPLO
 export const getAllDoctorsController = async (req: Request, res: Response) => {
     try {
-      const doctors = await getAllDoctors();
-      res.status(200).json({ success: true, data: doctors });
+        const doctors = await getAllDoctors();
+        res.status(200).json({ success: true, data: doctors });
     } catch (error) {
-      res.status(500).json({ success: false, message: 'Error retrieving doctors' });
+        res.status(500).json({ success: false, message: 'Error retrieving doctors' });
     }
 };
-// outros métodos como createDoctor, getDoctorById, updateDoctor, deleteDoctor
+
+export const createDoctorController = async (req: Request, res: Response) => {
+    try {
+        const { user_id, endereco, especialidade, aceita_dinheiro, aceita_plano } = req.body;
+        const newDoctor = await createDoctor({ user_id, endereco, especialidade, aceita_dinheiro, aceita_plano });
+        res.status(201).json({ success: true, data: newDoctor });
+    } catch (error) {
+        res.status(500).json({ success: false, message: 'Error creating Doctor' });
+    }
+};
+
+export const getdoctorByIdController = async (req: Request, res: Response) => {
+    try {
+        const { id } = req.params;
+        const doctor = await getdoctorById(id);
+        if (!doctor) {
+            return res.status(404).json({ success: false, message: 'doctor not found' });
+        }
+        res.status(200).json({ success: true, data: doctor });
+    } catch (error) {
+        res.status(500).json({ success: false, message: 'Error retrieving doctor' });
+    }
+};
+
+export const updatedoctorController = async (req: Request, res: Response) => {
+    try {
+        const { id } = req.params;
+        const updatedData = req.body;
+        const updateddoctor = await updatedoctor(id, updatedData);
+        if (!updateddoctor) {
+            return res.status(404).json({ success: false, message: 'doctor not found' });
+        }
+        res.status(200).json({ success: true, data: updateddoctor });
+    } catch (error) {
+        res.status(500).json({ success: false, message: 'Error updating doctor' });
+    }
+};
--- packages/server/src/services/doctorService.ts
+++ packages/server/src/services/doctorService.ts
@@ -1,10 +1,41 @@
 import Doctor from '../models/Doctor';
 
 export const getAllDoctors = async () => {
-  try {
-    return await Doctor.findAll();
-  } catch (error) {
-    throw new Error('Error retrieving doctors');
-  }
+    try {
+        return await Doctor.findAll();
+    } catch (error) {
+        throw new Error('Error retrieving doctors');
+    }
 };
-//Faz pra os outros ai...
+
+export const createDoctor = async (doctorData: { user_id: number; endereco: string; especialidade: string; aceita_dinheiro: boolean; aceita_plano: boolean }) => {
+    try {
+        return await Doctor.create(doctorData);
+    } catch (error) {
+        throw new Error('Error creating doctor');
+    }
+};
+
+export const getdoctorById = async (medico_id: string) => {
+    try {
+        const doctor = await Doctor.findByPk(medico_id);
+        if (!doctor) {
+            throw new Error('doctor not found');
+        }
+        return doctor;
+    } catch (error) {
+        throw new Error('Error retrieving doctor');
+    }
+};
+
+export const updatedoctor = async (medico_id: string, updatedData: { endereco?: string; especialidade?: string; aceita_dinheiro?: boolean; aceita_plano?: boolean }) => {
+    try {
+        const doctor = await Doctor.findByPk(medico_id);
+        if (!doctor) {
+            throw new Error('doctor not found');
+        }
+        return await doctor.update(updatedData);
+    } catch (error) {
+        throw new Error('Error updating doctor');
+    }
+};
