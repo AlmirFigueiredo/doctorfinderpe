4c97208afae85c2dcd2fa048e2a77fa7281a9ed9
Marcelo <mhbcoelho99@gmail.com>
2024-05-27 15:42:30 -0300

    feat: static profile page done

--- packages/web/doctorfinderpeweb/pages/[name].tsx
+++ packages/web/doctorfinderpeweb/pages/[name].tsx
@@ -1,9 +1,30 @@
 import { useRouter } from 'next/router'
 import styles from './profile.module.css'
+import { useState } from 'react';
+
+interface Address {
+    endereco: string;
+}
+
+interface FormDataTypes {
+    username?: string;
+    name?: string;
+    description?: string;
+    specialty?: string;
+    address?: Address[];
+
+}
 
 export default function Profile() {
     const router = useRouter()
     const perfilName = router.query.name;
+
+    const [formData, setFormData] = useState<FormDataTypes>({})
+
+    function handleUpdateProfile() {
+        
+    }
+
     return (
         <div className={styles.profile}>
             <header>
@@ -28,13 +49,42 @@ export default function Profile() {
                             </div>
                         </div>
                     </div>
-                    <div className={styles.username}>
-                        <span>Username</span>
-                        <div>
-                            <input type="text" placeholder='marcelocoelho1' />
+                    <form action="">
+                        <div className={styles.username}>
+                            <span>Username</span>
+                            <div>
+                                <input type="text" value="marcelocoelho1" placeholder='marcelocoelho1' />
+                            </div>
                         </div>
-                    </div>
-                    
+                        <div className={styles.userInfo}>
+                            <div>
+                                <label htmlFor="">Name</label>
+                                <input type="text" value="Marcelo Henrique" placeholder='Marcelo Henrique' />
+                            </div>
+                            <div>
+                                <label htmlFor="">Description</label>
+                                <textarea name="" rows={6} placeholder='description' id=""></textarea>
+                            </div>
+                            <div>
+                                <label htmlFor="">specialty</label>
+                                <input type="text" value="nutritionist" placeholder='nutritionist' />
+                            </div>
+                            <div>
+                                <div>
+                                    <label htmlFor="">address</label>
+                                </div>
+                                <section>
+                                    <input type="text" value="Rua xavier de brito" />
+                                    <input type="text" value="Rua xavier de brito" />
+                                </section>
+                            </div>
+                            <div>
+                                <span>Update profile</span>
+                                <button type='submit'>Update</button>
+                            </div>
+                        </div>
+                    </form>
+
                 </main>
             </div>
         </div>
