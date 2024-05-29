8625d0a827c06fd3662ceb12c1b1e90a44c4b748
Jose Almir <joseamfj@outlook.com>
2024-05-28 23:34:39 -0300

    Remove database url log

--- packages/server/src/config/database.ts
+++ packages/server/src/config/database.ts
@@ -2,7 +2,7 @@ import { Sequelize } from 'sequelize';
 import dotenv from 'dotenv';           
 
 dotenv.config(); 
-console.log(process.env.POSTGRES_URL); 
+//console.log(process.env.POSTGRES_URL); 
 const sequelize = new Sequelize(process.env.POSTGRES_URL as string, {
   dialect: 'postgres',
   dialectOptions: {
