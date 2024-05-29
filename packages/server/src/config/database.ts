433b901d939827382fb680845a0f8ec8221e1a17
Jose Almir <joseamfj@outlook.com>
2024-05-28 23:39:41 -0300

    Create configuration for Continuous Integration

--- packages/server/src/config/database.ts
+++ packages/server/src/config/database.ts
@@ -2,20 +2,10 @@ import { Sequelize } from 'sequelize';
 import dotenv from 'dotenv';           
 
 dotenv.config(); 
-//console.log(process.env.POSTGRES_URL); 
-const sequelize = new Sequelize(process.env.POSTGRES_URL as string, {
+const databaseUrl = process.env.POSTGRES_URL || 'postgresql://postgres:postgres@localhost:5432/postgres' 
+const sequelize = new Sequelize(databaseUrl, {
   dialect: 'postgres',
-  dialectOptions: {
-    // Esse ssl eh so pra producao, se for testar local, pode tirar
-    // ssl: {
-    //   require: true,
-    //   rejectUnauthorized: false
-    // }
-  }
 });
 
 export default sequelize; 
-//p rodar: abre o cmd no server, bota:
-//npm run dev
-//abre no browser: 
-//http://localhost:3000/
+
